0cb78ab68459cff8e850195d57d71c9a09672a28
unknown <joshuajulius2030@gmail.com>
2025-10-26 00:14:12 +0100

    mutables and immutables

---
+++ js/mutandimmutables.js
@@ -0,0 +1,13 @@
+// Mutables (Can be changed)
+let fruits = ["mango", "banana"]
+fruits.push("pear")
+fruits[0] = "fish"
+
+// Immutable (cannot be changed)
+let name = "Sophia"
+name[0] = "P"
+
+
+
+console.log(fruits)
+console.log(name)
