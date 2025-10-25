0702e0a41b333bbcf77b920a0ce10050aca8dfc9
unknown <joshuajulius2030@gmail.com>
2025-10-25 23:57:57 +0100

    data types

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let x = "john doe" //this is a string
+let y = 5 // number
+let z = false //boolean
+let object = ["banana", "apple", "pear"] //object
+let object2 = {"banana": "a fruit", "rice": "a food"}
+let nul = null //null
+let big = 90071998723487298347n //bigint
+console.log(typeof(z))
+console.log(z)
+console.log(object)
+console.log(typeof(big))
+console.log(object2["banana"])
+
