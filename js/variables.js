a411c85e56a658b282e335eb949473580e240de4
unknown <joshuajulius2030@gmail.com>
2025-10-26 00:06:12 +0100

    variable declaration

---
+++ js/variables.js
@@ -0,0 +1,15 @@
+let name = "influence"
+name = "John"
+// -------Only LET can be updated
+
+age = 33
+var age
+// Only VAR can allow you assign a value to a variable before defining the variable
+
+const course = "computer Science"
+console.log(name)
+console.log(age)
+
+
+// console.log(age)
+// console.log(course)
