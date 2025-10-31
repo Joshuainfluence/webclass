495b056524caf934e183bf94a168a6ccbb77cfbd
unknown <joshuajulius2030@gmail.com>
2025-10-31 03:49:04 +0100

    conditional statement and logical operator

---
+++ js/conditionals.js
@@ -0,0 +1,18 @@
+let cgpa = 8
+// LOGICAL OPERATORS
+// && and operator
+// || or operator
+if(cgpa >  5 || cgpa < 1.5 ){
+    console.log("Invalid grade")
+}
+else if(cgpa > 4 && cgpa < 5){
+    console.log("You are a first class student")
+}else if(cgpa > 3.5 && cgpa < 4.5){
+    console.log("You are a second class upper student")
+}else if(cgpa > 2.5 && cgpa < 3.5){
+    console.log("You are a second class lower student")
+}else{
+    console.log("YOu are a third class student")
+}
+
+
