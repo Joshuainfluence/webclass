df1a76eb22873f798ddf57298ff20b61e423db77
unknown <joshuajulius2030@gmail.com>
2025-11-14 14:58:41 +0100

    tenary operators

--- js/modulus.js
+++ js/modulus.js
@@ -3,7 +3,7 @@ a = 33
 
 let z = a % 2;
 
-// template literals
+// tenary operators and template literals
 const result = z === 0 ? `${a} is an even number` : `${a} is an odd number`
 console.log(result)
 
