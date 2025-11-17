b8f83897ba9053ed8b8808f81d063d1b575b1620
unknown <joshuajulius2030@gmail.com>
2025-11-17 10:24:19 +0100

    python arithmetic operators

---
+++ python/operators.py
@@ -0,0 +1,23 @@
+# arithmetic operators
+x = 10
+y = 20
+result = x + y
+# + for addition
+print(result)
+
+
+# - for substraction
+print(x -y)
+
+# / for division
+print(y / x)
+
+
+# for power
+print(x ** 3)
+
+# for modulus
+print(x % 4)
+
+# for multiplication
+print(x * y)
