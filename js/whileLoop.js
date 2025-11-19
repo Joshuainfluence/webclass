e9bd7b77b3b82513c17e61328eec79144ac02155
unknown <joshuajulius2030@gmail.com>
2025-11-19 11:06:46 +0100

    even number and odd number with python

---
+++ js/whileLoop.js
@@ -0,0 +1,24 @@
+// let i = 1
+// while (i<= 5){
+//     i++
+//     console.log(i)
+// }
+
+// let answer = ""
+
+// while(answer !== "yes"){
+//     answer = prompt("Do you want to exit this loop? Type 'yes' to exit")
+// }
+
+// let count = 0;
+// do {
+//     console.log("Count is: " + count);
+//     count++;
+// }while(count < 0);
+
+let x = 0;
+
+while (x < 100) {
+    x += Math.floor(Math.random() * 20);
+    console.log(x);
+}
---
+++ python/conditionals.py
@@ -0,0 +1,13 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+print(first_number + second_number)
+
+
+number = int(input("Enter the number to check: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
