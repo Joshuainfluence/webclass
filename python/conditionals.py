e91465853f39d254bd976a5122a52caf3b98271e
unknown <joshuajulius2030@gmail.com>
2025-11-19 11:50:43 +0100

    cgpa grading with python conditionals

--- python/conditionals.py
+++ python/conditionals.py
@@ -1,13 +1,26 @@
-first_number = int(input("Enter first number: "))
-second_number = int(input("Enter second number: "))
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
 
-print(first_number + second_number)
+# print(first_number + second_number)
 
 
-number = int(input("Enter the number to check: "))
+# number = int(input("Enter the number to check: "))
 
-if number % 2 == 0:
-    print(f"{number} is an even number")
+# if number % 2 == 0:
+#     print(f"{number} is an even number")
+# else:
+#     print(f"{number} is an odd number")
+
+
+
+
+cgpa = float(input("Enter your cpga: "))
+if(cgpa >= 4.5):
+    print("First class")
+elif(cgpa >= 3.5 and cgpa <= 4.4):
+    print("Second class upper")
+elif(cgpa >= 2.5 and cgpa <= 3.4):
+    print("Second class lower")
 else:
-    print(f"{number} is an odd number")
+    print("Third class")
 
