8574f720ce0522d0ffd628fcc616539c3331ffaa
unknown <joshuajulius2030@gmail.com>
2025-11-24 09:28:00 +0100

    for loop with conditional statement and inputs to identify odd and even number within the range

--- python/forloop.py
+++ python/forloop.py
@@ -2,12 +2,12 @@ startNumber  = int(input("Enter start number: "))
 stopNumber = int(input("Enter stop number: "))
 
 for i in range(startNumber, stopNumber):
-    if(i == stopNumber//2):
-        print(f"{i} You are halfway there!")
-    else:
-        print(i)
+    if i % 2 == 0:
+        print(f"{i} is an even number")
+    else: 
+        print(f"{i} is an odd number")
 
-        
+
 
 
 
