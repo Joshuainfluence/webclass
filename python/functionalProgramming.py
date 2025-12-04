5c86628fa86744a6c4335a41a117782274ede167
unknown <joshuajulius2030@gmail.com>
2025-12-04 09:48:09 +0100

    functional programming

---
+++ python/functionalProgramming.py
@@ -0,0 +1,8 @@
+square = lambda x: x**2
+print(square(3))
+
+greetings = lambda name: f"Good  morning {name}"
+print(greetings("INfluence"))
+
+evenNumber = lambda number: number % 2 == 0
+print(evenNumber(8))
