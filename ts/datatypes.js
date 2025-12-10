e3da84f8bda51357082b77deb46ed23ee2ce1185
unknown <joshuajulius2030@gmail.com>
2025-12-10 14:14:16 +0100

    ecommerce website header

---
+++ ts/datatypes.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return "Hello, ".concat(name);
+}
+console.log(greet("TypeScript"));
+// const log = (msg: string): void => console.log(msg);
+// log("This is a log message.");
