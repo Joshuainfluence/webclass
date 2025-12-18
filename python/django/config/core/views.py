bdfdb5e32575307fda47d09b50c60ad077666f89
unknown <joshuajulius2030@gmail.com>
2025-12-18 13:39:13 +0100

    about path

--- python/django/config/core/views.py
+++ python/django/config/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("This is the about page.")
